dad85560e4a6c56d3a824fd6106c5763bc189e54
wlthomson <will.tho@outlook.com>
2018-12-21 11:35:22 +1300

    Run server in electron main process, do not fork

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 const { app, BrowserWindow } = require("electron");
 
-const { fork } = require("child_process");
-
 const server = require("./server");
 
 function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600 });
   win.loadFile("build/index.html");
 
-  fork(server(app.getAppPath("userData")));
+  server([app.getAppPath("userData")]);
 }
 
 app.on("ready", createWindow);
