f6a532e696ac65db5495752636a8e193e705ba0d
wlthomson <will.tho@outlook.com>
2018-12-20 15:08:50 +1300

    Add minimal electron, react app

---
+++ src/Window.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Window extends Component {
+  render() {
+    return <div>Hello Window!</div>;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Window from "./Window";
+
+ReactDOM.render(<Window />, document.getElementById("index"));
