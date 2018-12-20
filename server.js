a7c406d1ceb3969e2c11786af0efc0b2a6f1b578
wlthomson <will.tho@outlook.com>
2018-12-20 21:12:37 +1300

    Add SQLite database

--- server.js
+++ server.js
@@ -3,6 +3,24 @@ const server = appPath => {
   const app = express();
   const port = 9615;
 
+  const sqlite3 = require("sqlite3").verbose();
+  const db = new sqlite3.Database(appPath + "/db.db");
+
+  db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS test (info text);");
+    db.run("INSERT INTO test VALUES ('Hello SQL!');");
+  });
+
+  app.get("/", (req, res) => {
+    console.log("Server received a request...");
+    db.get("SELECT * FROM test;", (err, row) => {
+      if (err) {
+        res.send(err);
+      }
+      res.send(row.info);
+    });
+  });
+
   app.get("/", (req, res) => res.send("Hello Server!"));
   app.listen(port, () => console.log(`Server listening on port ${port}...`));
 };
